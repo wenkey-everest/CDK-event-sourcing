d34c03471a488e2ffd3fe60dac31df7c0e07cd60
wenkey <muni.v.g@everest.engineering>
2022-05-28 22:09:10 +0530

    Added SQS for better optimization

--- lib/target-stack.ts
+++ lib/target-stack.ts
@@ -1,16 +1,38 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda_event_sources from 'aws-cdk-lib/aws-lambda-event-sources';
+import { SqsDestination } from 'aws-cdk-lib/aws-s3-notifications';
+
+
 
 export class TargetStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'Bucket', {
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TargetQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new sqs.Queue(this, 'Queue', {
+      visibilityTimeout: Duration.seconds(300),
+      receiveMessageWaitTime: Duration.seconds(20),
+    })
+
+    const lambdafunction = new lambda.Function(this, 'LambdaFunction', {
+      code: lambda.Code.fromAsset('src'),
+      handler: 'index.handler',
+      functionName: 'BucketPutHandler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+    })
+
+    bucket.addEventNotification(s3.EventType.OBJECT_CREATED_PUT, new SqsDestination(queue));
+
+    const sqsNotificationEventSource = new lambda_event_sources.SqsEventSource(queue)
+
+    lambdafunction.addEventSource(sqsNotificationEventSource)
   }
 }
