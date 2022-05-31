11045c6bca3895b2bfad00b3a0b33838870a92d0
wenkey <muni.v.g@everest.engineering>
2022-05-31 22:14:47 +0530

    Add save body and store in other bucket

--- lib/target-stack.ts
+++ lib/target-stack.ts
@@ -1,37 +1,48 @@
-import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import * as lambda_event_sources from 'aws-cdk-lib/aws-lambda-event-sources';
-import { SqsDestination } from 'aws-cdk-lib/aws-s3-notifications';
-
-
+import { Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as lambda_event_sources from "aws-cdk-lib/aws-lambda-event-sources";
+import { SqsDestination } from "aws-cdk-lib/aws-s3-notifications";
 
 export class TargetStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    const bucket = new s3.Bucket(this, 'Bucket', {
+    const inputBucket = new s3.Bucket(this, "input-bucket", {
       autoDeleteObjects: true,
       removalPolicy: RemovalPolicy.DESTROY
     })
 
-    const queue = new sqs.Queue(this, 'Queue', {
+    const storeBucket = new s3.Bucket(this, "store-bucket", { 
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY
+    })
+
+    
+    const queue = new sqs.Queue(this, "SQS", {
       visibilityTimeout: Duration.seconds(300),
       receiveMessageWaitTime: Duration.seconds(20),
     })
 
-    const lambdafunction = new lambda.Function(this, 'LambdaFunction', {
-      code: lambda.Code.fromAsset('src'),
-      handler: 'index.handler',
-      functionName: 'BucketPutHandler',
+    const lambdafunction = new lambda.Function(this, "lambdaFunction", {
+      code: lambda.Code.fromAsset("src"),
+      handler: "index.handler",
+      functionName: "target-stack-lambda-function",
       runtime: lambda.Runtime.NODEJS_12_X,
     })
 
-    bucket.addEventNotification(s3.EventType.OBJECT_CREATED_PUT, new SqsDestination(queue));
+    storeBucket.grantReadWrite(lambdafunction)
 
-    const sqsNotificationEventSource = new lambda_event_sources.SqsEventSource(queue)
+    inputBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED_PUT,
+      new SqsDestination(queue)
+    )
+
+    const sqsNotificationEventSource = new lambda_event_sources.SqsEventSource(
+      queue
+    )
 
     lambdafunction.addEventSource(sqsNotificationEventSource)
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-exports.handler = async (event) => {
-    event.Records.forEach((record) => {
-        // Prints over all body in the request
-            console.log(record.body)
-        })
-    }
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+exports.handler = (event) => {
+  event.Records.forEach((record) => {
+    const params = {
+      Bucket: "targetstack-storebuckete062d34b-qu340m9l0ahc",
+      Key: record.messageId + ".json",
+      ContentType: "application/json",
+      Body: record.body,
+    };
+    s3.putObject(params)
+      .promise()
+      .then((data) => {
+        console.log("complete:PUT Object", data);
+      })
+      .catch((err) => {
+        console.log("failure:PUT Object", err);
+      });
+  });
+};
